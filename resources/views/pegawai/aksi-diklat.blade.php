c7da6c6a0f268fd3258b347f72ea6b45bff5a1a5
ridhalfajri <ridhalfajri00@gmail.com>
2023-11-03 23:21:39 +0700

    Diklat : Update Button Aksi

--- resources/views/pegawai/aksi-diklat.blade.php
+++ resources/views/pegawai/aksi-diklat.blade.php
@@ -1,3 +1,20 @@
+{{-- <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+    <button type="button" class="btn btn-secondary"><i class="icon-eye" aria-hidden="true"></i></button>
+    <button type="button" class="btn btn-secondary"><i class="icon-eye" aria-hidden="true"></i></button>
+    <button type="button" class="btn btn-secondary"><i class="icon-eye" aria-hidden="true"></i></button>
+</div> --}}
+
+{{-- <a href="#" class="btn btn-sm btn-icon on-default"><i class="icon-eye" aria-hidden="true"></i></a>
 <a href="#" class="btn btn-sm btn-icon on-default"><i class="icon-eye" aria-hidden="true"></i></a>
-<a href="#" class="btn btn-sm btn-icon on-default"><i class="icon-eye" aria-hidden="true"></i></a>
-<a href="#" class="btn btn-sm btn-icon on-default"><i class="icon-eye" aria-hidden="true"></i></a>
+<a href="#" class="btn btn-sm btn-icon on-default"><i class="icon-eye" aria-hidden="true"></i></a> --}}
+<div class="item-action dropdown ml-2">
+    <a href="javascript:void(0)" data-toggle="dropdown"><i class="fe fe-align-justify"></i></a>
+    <div class="dropdown-menu dropdown-menu-right">
+        <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fe fe-eye"></i>
+            Lihat</a>
+        <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fa fa-edit"></i>
+            Edit</a>
+        <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fe fe-trash-2"></i>
+            Hapus</a>
+    </div>
+</div>
