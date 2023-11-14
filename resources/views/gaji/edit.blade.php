30b3031580ae98b53a5d3e75f3f65969c6c2e910
teguh-ri <35196927+teguh-ri@users.noreply.github.com>
2023-11-14 12:36:32 +0700

    Update Gaji

--- resources/views/gaji/edit.blade.php
+++ resources/views/gaji/edit.blade.php
@@ -5,12 +5,12 @@
 @endpush
 
 @push('breadcrumb')
-    <div class="btn-group btn-breadcrumb">
-        <a href="/" class="btn btn-primary"><i class="fa fa-home"></i></a>
-        <a href="/gaji" class="btn btn-info"><i class="fa fa-list"></i> Gaji</a>
-        <a href="#" class="btn btn-warning"><i class="fa fa-plus"></i> Ubah Data Gaji</a>
-        {{-- <a href="/gaji" class="btn btn-outline-danger"><i class="fa fa-chevron-circle-left"></i> Kembali</a> --}}
-    </div>
+        <div class="btn-group btn-breadcrumb">
+            <a href="/" class="btn btn-primary"><i class="fa fa-home"></i></a>
+            <a href="{{ route('gaji.index') }}" class="btn btn-info"><i class="fa fa-list"></i> Gaji</a>
+            <a href="#" class="btn"><i class="fa fa-pencil-square" aria-hidden="true"></i> Ubah Gaji Baru</a>
+            {{-- <a href="/" class="btn btn-outline-danger"><i class="fa fa-chevron-circle-left"></i> Kembali</a> --}}
+        </div>
 @endpush
 
 @section('content')
@@ -18,7 +18,7 @@
         <div class="card">
             <div class="card-body">
                 <div class="card-content">
-                    <form id="gajiForm" method="post"  action="{{ route('gaji.update',$gaji->id) }}"  accept-charset="utf-8">
+                    <form class="needs-validation" id="gajiForm" method="post"  action="{{ route('gaji.update',$gaji->id) }}"  accept-charset="utf-8" novalidate>
                         @csrf
                         @method('PUT')
 
@@ -26,12 +26,15 @@
                             <div class="col-12 col-lg-12 col-md-12">
                                 <div class="form-group @error('golongan_id') has-error @enderror">
                                     <label>Golongan :<span class="text-danger"><sup>*</sup></span></label>
-                                    <select class="form-control" id="golongan_id" name="golongan_id">
+                                    <select class="form-control" id="golongan_id" name="golongan_id" required>
                                         <option value="" selected disabled>Pilih Golongan</option>
                                         @foreach ($golongan as $data)
-                                            <option value="{{ $data->id }}" {{ (old('golongan_id') ?? $gaji->golongan_id)  == $data->id ? 'selected' : '' }}>{{ $data->nama }}</option>
+                                            <option value="{{ $data->id }}" {{ (old('golongan_id') ?? $gaji->golongan_id)  == $data->id ? 'selected' : '' }}>{{ "Gol: " . $data->nama . "  " . $data->nama_pangkat }}</option>
                                         @endforeach
                                     </select>
+                                    <div class="invalid-feedback">
+                                        Silakan pilih Golongan.
+                                    </div>
                                     @error('golongan_id')
                                         <small class="text-danger">{{ $message }}</small>
                                     @enderror
@@ -43,7 +46,7 @@
                             <div class="col-12 col-lg-12 col-md-12">
                                 <div class="form-group  @error('masa_kerja') has-error @enderror">
                                     <label>Masa Kerja<span class="text-danger"><sup>*</sup></span></label>
-                                    <input class="form-control"  id="masa_kerja" name = "masa_kerja" value = "{{ old('masa_kerja') ?? $gaji->masa_kerja }}"" placeholder="Masa Kerja" autocomplete="off"/>
+                                    <input class="form-control" type="number" id="masa_kerja" name = "masa_kerja" value = "{{ old('masa_kerja') ?? $gaji->masa_kerja }}" placeholder="Masa Kerja" autocomplete="off" required/>
                                     @error('masa_kerja')
                                         <small class="text-danger">{{ $message }}</small>
                                     @enderror
@@ -55,7 +58,7 @@
                             <div class="col-12 col-lg-12 col-md-12">
                                 <div class="form-group  @error('nominal') has-error @enderror">
                                     <label>Nominal<span class="text-danger"><sup>*</sup></span></label>
-                                    <input class="form-control"  id="nominal" name = "nominal" value = "{{ old('nominal') ?? $gaji->nominal }}"" placeholder="Nominal" autocomplete="off"/>
+                                    <input class="form-control" id="nominal" name = "nominal" value = "{{ number_format(preg_replace('/[^\d]/', '', (old('nominal') ?? $gaji->nominal)), 0, ',', '.')  }}" placeholder="Nominal" autocomplete="off" required/>
                                     @error('nominal')
                                         <small class="text-danger">{{ $message }}</small>
                                     @enderror
@@ -63,7 +66,7 @@
                             </div>
                         </div>
 
-                        <button type="submit" class="btn btn-primary btn-sm waves-effect waves-light">Ubah</button>
+                        <button type="submit" class="btn btn-primary btn-sm waves-effect waves-light"><i class="fa fa-check-square" aria-hidden="true"></i> Simpan Perubahan</button>
                     </form>
                 </div>
 
@@ -74,31 +77,59 @@
 @push('script')
     <script src="{{ asset('assets/plugins/sweetalert/sweetalert.min.js') }}"></script>
     <script src="{{ asset('assets/plugins/select2-4.0.13/dist/js/select2.min.js') }}"></script>
-     <script>
+    <script>
+        (function () {
+          'use strict'
+
+          var forms = document.querySelectorAll('.needs-validation')
+
+          Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+              form.addEventListener('submit', function (event) {
+                event.preventDefault()
+                if (!form.checkValidity()) {
+                  event.stopPropagation()
+                  form.classList.add('was-validated')
+                }else
+                {
+                    // Konfirmasi sebelum mengubah data
+                    swal({
+                        title: 'Konfirmasi!',
+                        text: 'Apakah anda yakin ingin mengubah data?',
+                        type: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#78c0ec',
+                        confirmButtonText: 'Ya',
+                        cancelButtonText: 'Tidak',
+                        closeOnConfirm: true,
+                        closeOnCancel: false
+                    }, function(isConfirm) {
+                        if (isConfirm) {
+                            form.submit()
+                        } else {
+                            swal('Informasi', 'Ubah data dibatalkan', 'error');
+                            event.stopPropagation()
+                        }
+                    });
+                }
+              }, false)
+            });
+        })();
+
         $(document).ready(function() {
             $('#golongan_id').select2();
+            $("#nominal").on("keyup change", function(e) {
+                // Ambil nilai teks dari input
+                let inputValue =  $("#nominal").val();
+                // Hapus semua karakter non-angka
+                inputValue = inputValue.replace(/\D/g, '');
+                // Format angka menjadi format mata uang Indonesia
+                let formattedValue = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(inputValue);
+                console.log(formattedValue);
+                $("#nominal").val(formattedValue);
+            })
         });
-        $('#gajiForm').on('submit', function(e) {
-            e.preventDefault();
 
-            swal({
-                    title: 'Konfirmasi!',
-                    text: 'Apakah anda yakin ingin mengubah data?',
-                    type: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#78c0ec',
-                    confirmButtonText: 'Ya',
-                    cancelButtonText: 'Tidak',
-                    closeOnConfirm: true,
-                    closeOnCancel: false
-                }, function(isConfirm) {
-                    if (isConfirm) {
-                        $('#gajiForm').off('submit').submit();
-                    } else {
-                        swal('Informasi', 'Ubah data dibatalkan', 'error');
-                    }
-                });
-        });
 
     </script>
 @endpush
